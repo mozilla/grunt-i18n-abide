84f4db11cc7e419dd1d9a23755a313bb30a650d3
Stuart Colville <scolville@mozilla.com>
2013-11-08 21:03:51 +0000

    Actually add create task

---
+++ tasks/jsxcreate.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var shell = require('shelljs');
+
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.registerTask('jsxcreate', 'Wraps msginit to simplify locale creation.', function () {
+
+    var options = this.options();
+    var baseLocaleDir = options.baseDir || 'locale';
+
+    var spawnOpts = {
+      stdio: ['pipe', null, null, null, 'pipe']
+    };
+
+    if (options.cwd) {
+      spawnOpts.cwd = options.cwd;
+    }
+
+    var template = options.template;
+    template = template || path.join(baseLocaleDir, 'templates/LC_MESSAGES/messages.pot');
+    template = path.normalize(template);
+
+    if (!grunt.file.isFile(template)) {
+      grunt.log.error('template file "' + template + '" does not exist');
+      return false;
+    }
+
+    options.locales.forEach(function(locale) {
+      // Make the dir for the locale.
+      var args = [];
+      var outputFile = path.join(baseLocaleDir, locale, 'LC_MESSAGES/messages.po');
+
+      args.push('msginit');
+
+      // Non-interactive :)
+      args.push('--no-translator');
+
+      args.push('--input');
+      args.push(template);
+
+      args.push('--output');
+      args.push(outputFile);
+
+      grunt.file.mkdir(path.join(baseLocaleDir, locale, 'LC_MESSAGES'));
+
+      args.push('-l');
+      args.push(locale);
+
+      // Synchronously execute commands.
+      shell.exec(args.join(' '));
+
+      grunt.log.ok('Locale "' + locale + '" created successfully.');
+    });
+
+  });
+
+};
